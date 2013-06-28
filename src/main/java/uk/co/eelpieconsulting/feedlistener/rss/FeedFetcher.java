04046607ec52c296d1436d55cb97f6e234d3e7c4
Tony McCrae <tony@eelpieconsulting.co.uk>
2013-06-28 21:01:24 +0100

    Refactor; mapping from given format to feed items should be seperate classes.

--- src/main/java/uk/co/eelpieconsulting/feedlistener/rss/FeedFetcher.java
+++ src/main/java/uk/co/eelpieconsulting/feedlistener/rss/FeedFetcher.java
@@ -2,21 +2,15 @@ package uk.co.eelpieconsulting.feedlistener.rss;
 
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import uk.co.eelpieconsulting.common.geo.model.LatLong;
 import uk.co.eelpieconsulting.feedlistener.model.FeedItem;
 
-import com.sun.syndication.feed.module.georss.GeoRSSModule;
-import com.sun.syndication.feed.module.georss.GeoRSSUtils;
-import com.sun.syndication.feed.module.mediarss.MediaEntryModuleImpl;
-import com.sun.syndication.feed.module.mediarss.MediaModule;
-import com.sun.syndication.feed.module.mediarss.types.MediaContent;
 import com.sun.syndication.feed.synd.SyndEntry;
 import com.sun.syndication.feed.synd.SyndFeed;
 import com.sun.syndication.fetcher.impl.HttpURLFeedFetcher;
@@ -26,6 +20,13 @@ public class FeedFetcher {
 	
 	private static Logger log = Logger.getLogger(FeedFetcher.class);
 	
+	private RssFeedItemMapper rssFeedItemMapper;
+	
+	@Autowired
+	public FeedFetcher(RssFeedItemMapper rssFeedItemMapper) {
+		this.rssFeedItemMapper = rssFeedItemMapper;
+	}
+
 	@SuppressWarnings("unchecked")
 	public FetchedFeed fetchFeed(String url) {
 		final List<FeedItem> feedItems = new ArrayList<FeedItem>();
@@ -38,12 +39,7 @@ public class FeedFetcher {
         Iterator<SyndEntry> feedItemsIterator = syndfeed.getEntries().iterator();
         while (feedItemsIterator.hasNext()) {        	
         	final SyndEntry syndEntry = (SyndEntry) feedItemsIterator.next();
-        	final LatLong latLong = extractLocationFrom(syndEntry);        	
-			final String imageUrl = extractImageFrom(syndEntry);			
-        	final String body = syndEntry.getDescription() != null ? syndEntry.getDescription().getValue() : null;
-			final Date date = syndEntry.getPublishedDate() != null ? syndEntry.getPublishedDate() : syndEntry.getUpdatedDate();
-						
-			feedItems.add(new FeedItem(syndEntry.getTitle(), syndEntry.getLink(), body, date, latLong, imageUrl));
+        	feedItems.add(rssFeedItemMapper.createFeedItemFrom(syndEntry));
         }
         
         return new FetchedFeed(syndfeed.getTitle(), feedItems);
@@ -59,57 +55,6 @@ public class FeedFetcher {
 			log.warn("Error while fetching feed: " + e.getMessage());
 		}
 		return null;
-	}
-	
-	private LatLong extractLocationFrom(SyndEntry syndEntry) {
-		final GeoRSSModule geoModule = (GeoRSSModule) GeoRSSUtils.getGeoRSS(syndEntry);
-		if (geoModule != null && geoModule.getPosition() != null) {
-			final LatLong latLong = new LatLong(geoModule.getPosition().getLatitude(), geoModule.getPosition().getLongitude());
-			log.debug("Rss item '" + syndEntry.getTitle() + "' has position information: " + latLong);
-			return latLong;
-		}
-		return null;
-	}
-	
-	private String extractImageFrom(SyndEntry item) {
-		final MediaEntryModuleImpl mediaModule = (MediaEntryModuleImpl) item.getModule(MediaModule.URI);
-		if (mediaModule == null) {
-			log.debug("No media module found for item: " + item.getTitle());
-			return null;
-		}
-		
-		final MediaContent[] mediaContents = mediaModule.getMediaContents();
-		if (mediaContents.length > 0) {			
-			MediaContent selectedMediaContent = null;
-			for (int i = 0; i < mediaContents.length; i++) {
-				MediaContent mediaContent = mediaContents[i];
-				final boolean isImage = isImage(mediaContent);
-				if (isImage && isBetterThanCurrentlySelected(mediaContent, selectedMediaContent)) {
-					selectedMediaContent = mediaContent;
-				}
-			}
-			
-			if (selectedMediaContent != null) {
-				log.debug("Took image reference from MediaContent: " + selectedMediaContent.getReference().toString());
-				return selectedMediaContent.getReference().toString();
-			}
-		}
-		
-		log.debug("No suitable media element image seen");
-		return null;
-	}
-
-	private boolean isImage(MediaContent mediaContent) {
-		final boolean hasTypeJpegAttribute = mediaContent.getType() != null && mediaContent.getType().equals("image/jpeg");
-		final boolean isJpegUrl = mediaContent.getReference() != null && mediaContent.getReference().toString().endsWith("jpg");	 // TODO test cover and .
-		return mediaContent.getReference() != null && (hasTypeJpegAttribute || isJpegUrl);
-	}
-	
-	private boolean isBetterThanCurrentlySelected(MediaContent mediaContent, MediaContent selectedMediaContent) {
-		if (selectedMediaContent == null) {
-			return true;
-		}		
-		return mediaContent.getWidth() != null && mediaContent.getWidth() > selectedMediaContent.getWidth();		
 	}
 	
 }
---
+++ src/main/java/uk/co/eelpieconsulting/feedlistener/rss/RssFeedItemMapper.java
@@ -0,0 +1,83 @@
+package uk.co.eelpieconsulting.feedlistener.rss;
+
+import java.util.Date;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Component;
+
+import uk.co.eelpieconsulting.common.geo.model.LatLong;
+import uk.co.eelpieconsulting.feedlistener.model.FeedItem;
+
+import com.sun.syndication.feed.module.georss.GeoRSSModule;
+import com.sun.syndication.feed.module.georss.GeoRSSUtils;
+import com.sun.syndication.feed.module.mediarss.MediaEntryModuleImpl;
+import com.sun.syndication.feed.module.mediarss.MediaModule;
+import com.sun.syndication.feed.module.mediarss.types.MediaContent;
+import com.sun.syndication.feed.synd.SyndEntry;
+
+@Component
+public class RssFeedItemMapper {
+	
+	private static Logger log = Logger.getLogger(RssFeedItemMapper.class);
+	
+	public FeedItem createFeedItemFrom(final SyndEntry syndEntry) {
+		final LatLong latLong = extractLocationFrom(syndEntry);        	
+		final String imageUrl = extractImageFrom(syndEntry);			
+		final String body = syndEntry.getDescription() != null ? syndEntry.getDescription().getValue() : null;
+		final Date date = syndEntry.getPublishedDate() != null ? syndEntry.getPublishedDate() : syndEntry.getUpdatedDate();						
+		final FeedItem feedItem = new FeedItem(syndEntry.getTitle(), syndEntry.getLink().trim(), body, date, latLong, imageUrl);
+		return feedItem;
+	}
+	
+	private LatLong extractLocationFrom(SyndEntry syndEntry) {
+		final GeoRSSModule geoModule = (GeoRSSModule) GeoRSSUtils.getGeoRSS(syndEntry);
+		if (geoModule != null && geoModule.getPosition() != null) {
+			final LatLong latLong = new LatLong(geoModule.getPosition().getLatitude(), geoModule.getPosition().getLongitude());
+			log.debug("Rss item '" + syndEntry.getTitle() + "' has position information: " + latLong);
+			return latLong;
+		}
+		return null;
+	}
+	
+	private String extractImageFrom(SyndEntry item) {
+		final MediaEntryModuleImpl mediaModule = (MediaEntryModuleImpl) item.getModule(MediaModule.URI);
+		if (mediaModule == null) {
+			log.debug("No media module found for item: " + item.getTitle());
+			return null;
+		}
+		
+		final MediaContent[] mediaContents = mediaModule.getMediaContents();
+		if (mediaContents.length > 0) {			
+			MediaContent selectedMediaContent = null;
+			for (int i = 0; i < mediaContents.length; i++) {
+				MediaContent mediaContent = mediaContents[i];
+				final boolean isImage = isImage(mediaContent);
+				if (isImage && isBetterThanCurrentlySelected(mediaContent, selectedMediaContent)) {
+					selectedMediaContent = mediaContent;
+				}
+			}
+			
+			if (selectedMediaContent != null) {
+				log.debug("Took image reference from MediaContent: " + selectedMediaContent.getReference().toString());
+				return selectedMediaContent.getReference().toString();
+			}
+		}
+		
+		log.debug("No suitable media element image seen");
+		return null;
+	}
+	
+	private boolean isImage(MediaContent mediaContent) {
+		final boolean hasTypeJpegAttribute = mediaContent.getType() != null && mediaContent.getType().equals("image/jpeg");
+		final boolean isJpegUrl = mediaContent.getReference() != null && mediaContent.getReference().toString().endsWith("jpg");	 // TODO test cover and .
+		return mediaContent.getReference() != null && (hasTypeJpegAttribute || isJpegUrl);
+	}
+	
+	private boolean isBetterThanCurrentlySelected(MediaContent mediaContent, MediaContent selectedMediaContent) {
+		if (selectedMediaContent == null) {
+			return true;
+		}		
+		return mediaContent.getWidth() != null && mediaContent.getWidth() > selectedMediaContent.getWidth();		
+	}
+
+}
