e351f2260237331430234236efecac884d4996f5
Tony McCrae <tony@eelpieconsulting.co.uk>
2013-06-28 21:01:40 +0100

    Display inbox on homepage.

--- src/test/java/uk/co/eelpieconsulting/feedlistener/controllers/HomepageController.java
+++ src/test/java/uk/co/eelpieconsulting/feedlistener/controllers/HomepageController.java
@@ -6,22 +6,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+
+import uk.co.eelpieconsulting.feedlistener.daos.FeedItemDAO;
 import uk.co.eelpieconsulting.feedlistener.daos.SubscriptionsDAO;
 
 @Controller
 public class HomepageController {
 	
-	private SubscriptionsDAO subscriptionsDAO;
+	private final SubscriptionsDAO subscriptionsDAO;
+	private final FeedItemDAO feedItemDAO;
 	
 	@Autowired
-	public HomepageController(SubscriptionsDAO subscriptionsDAO) {
+	public HomepageController(SubscriptionsDAO subscriptionsDAO, FeedItemDAO feedItemDAO) {
 		this.subscriptionsDAO = subscriptionsDAO;
+		this.feedItemDAO = feedItemDAO;
 	}
 	
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public ModelAndView homepage() {
 		final ModelAndView mv = new ModelAndView("homepage");
 		mv.addObject("subscriptions", subscriptionsDAO.getSubscriptions());
+		mv.addObject("inbox", Lists.newArrayList(Iterables.limit(feedItemDAO.getAll(), 10)));
 		return mv;
 	}
 }
