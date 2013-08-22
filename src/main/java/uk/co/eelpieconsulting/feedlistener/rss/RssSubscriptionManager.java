eb5d2341395a87bc7090b712870845d25212787c
Tony McCrae <tony@eelpieconsulting.co.uk>
2013-08-22 15:11:03 +1200

    Autowiring.

--- src/main/java/uk/co/eelpieconsulting/feedlistener/rss/RssSubscriptionManager.java
+++ src/main/java/uk/co/eelpieconsulting/feedlistener/rss/RssSubscriptionManager.java
@@ -1,7 +1,10 @@
 package uk.co.eelpieconsulting.feedlistener.rss;
 
+import org.springframework.stereotype.Component;
+
 import uk.co.eelpieconsulting.feedlistener.model.RssSubscription;
 
+@Component
 public class RssSubscriptionManager {
 	
 	public RssSubscription requestFeedSubscription(String url, String channel) {
