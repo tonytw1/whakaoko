814a57e1292cf82c7eaedee019a5993a8e5924ad
Tony McCrae <tony@eelpieconsulting.co.uk>
2013-12-05 21:40:03 +0000

    Timing aspect.

---
+++ src/main/java/uk/co/eelpieconsulting/feedlistener/annotations/TimingAspect.java
@@ -0,0 +1,52 @@
+package uk.co.eelpieconsulting.feedlistener.annotations;
+
+import java.lang.reflect.Method;
+
+import org.apache.log4j.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+
+@Aspect
+public class TimingAspect {
+	
+	private static Logger log = Logger.getLogger(TimingAspect.class);
+
+	/**
+	 * This around advice adds timing to any method annotated with the Timed
+	 * annotation. It binds the annotation to the parameter timedAnnotation so
+	 * that the values are available at runtime. Also note that the retention
+	 * policy of the annotation needs to be RUNTIME.
+	 * 
+	 * @param pjp
+	 *            - the join point for this advice
+	 * @param timedAnnotation
+	 *            - the Timed annotation as declared on the method
+	 * @return
+	 * @throws Throwable
+	 */
+	@Around("@annotation( timedAnnotation ) ")
+	public Object processSystemRequest(final ProceedingJoinPoint pjp, Timed timedAnnotation) throws Throwable {		
+		try {
+			long start = System.currentTimeMillis();
+			Object retVal = pjp.proceed();
+			long end = System.currentTimeMillis();
+			long differenceMs = end - start;
+
+			final MethodSignature methodSignature = (MethodSignature) pjp.getSignature();
+			final Method targetMethod = methodSignature.getMethod();			
+			log.info(targetMethod.getDeclaringClass().getName() + "."
+					+ targetMethod.getName() + " took " + differenceMs
+					+ " ms : timing notes: " + timedAnnotation.timingNotes()
+					+ " request info : ");
+			
+			return retVal;
+			
+		} catch (Exception e) {
+			log.error(e);
+			throw e;
+		}
+	}
+
+}
