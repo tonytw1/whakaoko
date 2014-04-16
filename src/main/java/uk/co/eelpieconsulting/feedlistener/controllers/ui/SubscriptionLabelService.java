ff296a01fb6cbb76d89677ab8b7462f032423652
Tony McCrae <tony@eelpieconsulting.co.uk>
2014-04-16 20:10:25 +1200

    Render labels for feed item subscription.

---
+++ src/main/java/uk/co/eelpieconsulting/feedlistener/controllers/ui/SubscriptionLabelService.java
@@ -0,0 +1,29 @@
+package uk.co.eelpieconsulting.feedlistener.controllers.ui;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import uk.co.eelpieconsulting.feedlistener.UnknownSubscriptionException;
+import uk.co.eelpieconsulting.feedlistener.daos.SubscriptionsDAO;
+
+@Component
+public class SubscriptionLabelService {
+
+	private final SubscriptionsDAO subscriptionsDAO;
+	private final CurrentUserService currentUserService;
+
+	@Autowired
+	public SubscriptionLabelService(CurrentUserService currentUserService, SubscriptionsDAO subscriptionsDAO) {
+		this.currentUserService = currentUserService;
+		this.subscriptionsDAO = subscriptionsDAO;
+	}
+	
+	public String label(String subscriptionId) {
+		try {
+			return subscriptionsDAO.getById(currentUserService.getCurrentUser(), subscriptionId).getName();
+		} catch (UnknownSubscriptionException e) {
+			return subscriptionId.toUpperCase();
+		}		
+	}
+	
+}
