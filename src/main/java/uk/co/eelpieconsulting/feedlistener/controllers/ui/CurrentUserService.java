a4a439ca3a6e97871ffbd34dde9e7308e142bf1d
Tony McCrae <tony@eelpieconsulting.co.uk>
2014-04-16 18:45:58 +1200

    Views use user specific urls.

---
+++ src/main/java/uk/co/eelpieconsulting/feedlistener/controllers/ui/CurrentUserService.java
@@ -0,0 +1,22 @@
+package uk.co.eelpieconsulting.feedlistener.controllers.ui;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CurrentUserService {
+	
+	private final HttpServletRequest request;
+		
+	@Autowired	
+	public CurrentUserService(HttpServletRequest request) {
+		this.request = request;
+	}
+	
+	public String getCurrentUser() {
+		return request.getPathInfo().split("/")[2];
+	}
+	
+}
