e1f85965a3747cd9079de496f9a7ea98b0b84fb9
Tony McCrae <tony@eelpieconsulting.co.uk>
2014-04-16 19:47:04 +1200

    Instagram oauth is multi user.

--- src/main/java/uk/co/eelpieconsulting/feedlistener/instagram/auth/InstagramOauthController.java
+++ src/main/java/uk/co/eelpieconsulting/feedlistener/instagram/auth/InstagramOauthController.java
@@ -6,6 +6,7 @@ import org.apache.log4j.Logger;
 import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,7 +25,7 @@ import uk.co.eelpieconsulting.feedlistener.instagram.api.InstagramApi;
 @Controller
 public class InstagramOauthController {
 	
-	private static Logger log = Logger.getLogger(InstagramOauthController.class);
+	private final static Logger log = Logger.getLogger(InstagramOauthController.class);
 	
 	private static final String INSTAGRAM_OAUTH_CALLBACK = "/instagram/oauth";
 	
@@ -39,25 +40,25 @@ public class InstagramOauthController {
 		this.instagramApi = new InstagramApi();
 	}
 	
-	@RequestMapping(value="/instagram/authorise", method=RequestMethod.GET)
-	public ModelAndView authorize() {
-		final String authorizeRedirectUrl = authorizeRedirectUrl();
+	@RequestMapping(value="/instagram/authorise/{username}", method=RequestMethod.GET)
+	public ModelAndView authorize(@PathVariable String username) {
+		final String authorizeRedirectUrl = authorizeRedirectUrl(username);
 		log.info("Redirecting user to instagram: " + authorizeRedirectUrl);
 		return new ModelAndView(new RedirectView(authorizeRedirectUrl));
 	}
 	
 	@RequestMapping(value=INSTAGRAM_OAUTH_CALLBACK, method=RequestMethod.GET)
-	public ModelAndView dataCallback(@RequestParam String code) throws IOException, HttpNotFoundException, HttpBadRequestException, HttpForbiddenException, HttpFetchException, JSONException, UnknownUserException {		
+	public ModelAndView dataCallback(@PathVariable String username, @RequestParam String code) throws IOException, HttpNotFoundException, HttpBadRequestException, HttpForbiddenException, HttpFetchException, JSONException, UnknownUserException {		
 		log.info("Received oauth callback code: " + code);
 		
 		log.info("Exchanging oauth callback code for access token");
-		final String authorizeRedirectUrl = authorizeRedirectReturnUrl();
+		final String authorizeRedirectUrl = authorizeRedirectReturnUrl(username);
 		log.info(authorizeRedirectUrl);
 		try {
 			final String accessToken = instagramApi.getAccessToken(credentialService.getInstagramClientId(), credentialService.getInstagramClientSecret(), code, authorizeRedirectUrl);
 			
 			log.info("Got instagram access token: " + accessToken);
-			credentialService.setInstagramAccessTokenForUser("tonytw1", accessToken);	// TODO username
+			credentialService.setInstagramAccessTokenForUser(username, accessToken);
 			
 		} catch (HttpBadRequestException e) {
 			log.error(e.getResponseBody());
@@ -65,12 +66,12 @@ public class InstagramOauthController {
 		return null;
 	}
 	
-	private String authorizeRedirectUrl() {
-		return instagramApi.getAuthorizeRedirectUrl(credentialService.getInstagramClientId(), authorizeRedirectReturnUrl());
+	private String authorizeRedirectUrl(String username) {
+		return instagramApi.getAuthorizeRedirectUrl(credentialService.getInstagramClientId(), authorizeRedirectReturnUrl(username));
 	}
 
-	private String authorizeRedirectReturnUrl() {
-		return urlBuilder.getBaseUrl() + INSTAGRAM_OAUTH_CALLBACK;
+	private String authorizeRedirectReturnUrl(String username) {
+		return urlBuilder.getBaseUrl() + INSTAGRAM_OAUTH_CALLBACK + "/" + username;
 	}
 	
 }
