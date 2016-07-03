3de488b392e464fd42755e097022bc01b7821538
Tony McCrae <tony@eelpieconsulting.co.uk>
2016-07-03 19:43:39 +0100

    Mongo SSL connection flag.

--- src/main/java/uk/co/eelpieconsulting/feedlistener/daos/DataStoreFactory.java
+++ src/main/java/uk/co/eelpieconsulting/feedlistener/daos/DataStoreFactory.java
@@ -1,25 +1,20 @@
 package uk.co.eelpieconsulting.feedlistener.daos;
 
-import java.net.UnknownHostException;
-import java.util.List;
-
+import com.google.common.collect.Lists;
+import com.mongodb.*;
 import org.apache.log4j.Logger;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
 import uk.co.eelpieconsulting.feedlistener.model.Channel;
 import uk.co.eelpieconsulting.feedlistener.model.FeedItem;
 import uk.co.eelpieconsulting.feedlistener.model.Subscription;
 import uk.co.eelpieconsulting.feedlistener.model.User;
 
-import com.google.common.collect.Lists;
-import com.mongodb.MongoClient;
-import com.mongodb.MongoCredential;
-import com.mongodb.MongoException;
-import com.mongodb.ServerAddress;
+import java.net.UnknownHostException;
+import java.util.List;
 
 @Component
 public class DataStoreFactory {
@@ -28,6 +23,7 @@ public class DataStoreFactory {
 	    
 	private final ServerAddress serverAddress;
 	private final String mongoDatabase;
+	private final MongoClientOptions mongoClientOptions;
 	
 	private final List<MongoCredential> credentials;
 
@@ -37,11 +33,15 @@ public class DataStoreFactory {
 	public DataStoreFactory(@Value("#{config['mongoHost']}") String mongoHost,
 			@Value("#{config['mongoDatabase']}") String mongoDatabase,
 			@Value("#{config['mongoUser']}") String mongoUser,
-			@Value("#{config['mongoPassword']}") String mongoPassword) throws UnknownHostException, MongoException {
-		this.serverAddress = new ServerAddress(mongoHost);
+			@Value("#{config['mongoPassword']}") String mongoPassword,
+			@Value("#{config['mongoSSL']}") Boolean mongoSSL) throws UnknownHostException, MongoException {
 		this.mongoDatabase = mongoDatabase;
+
 		MongoCredential createCredential = MongoCredential.createCredential(mongoUser, mongoDatabase, mongoPassword.toCharArray());
 		this.credentials = Lists.newArrayList(createCredential);
+
+		this.mongoClientOptions = MongoClientOptions.builder().sslEnabled(mongoSSL).build();
+		this.serverAddress = new ServerAddress(mongoHost);
 	}
 	
 	public Datastore getDs() {
@@ -60,7 +60,7 @@ public class DataStoreFactory {
 		morphia.map(User.class);
 		
 		try {
-			MongoClient m = new MongoClient(serverAddress, credentials);
+			MongoClient m = new MongoClient(serverAddress, credentials, mongoClientOptions);
 			return morphia.createDatastore(m, database);
 			
 		} catch (MongoException e) {
