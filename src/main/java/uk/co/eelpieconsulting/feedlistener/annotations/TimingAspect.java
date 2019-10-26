b1a7cbf793747f8059bf1be4ee2d7718a23ae102
Tony McCrae <tony@eelpieconsulting.co.uk>
2019-10-26 09:43:21 +0100

    Only log slow queries.

--- src/main/java/uk/co/eelpieconsulting/feedlistener/annotations/TimingAspect.java
+++ src/main/java/uk/co/eelpieconsulting/feedlistener/annotations/TimingAspect.java
@@ -10,43 +10,49 @@ import org.aspectj.lang.reflect.MethodSignature;
 
 @Aspect
 public class TimingAspect {
-	
-	private static Logger log = Logger.getLogger(TimingAspect.class);
 
-	/**
-	 * This around advice adds timing to any method annotated with the Timed
-	 * annotation. It binds the annotation to the parameter timedAnnotation so
-	 * that the values are available at runtime. Also note that the retention
-	 * policy of the annotation needs to be RUNTIME.
-	 * 
-	 * @param pjp
-	 *            - the join point for this advice
-	 * @param timedAnnotation
-	 *            - the Timed annotation as declared on the method
-	 * @return
-	 * @throws Throwable
-	 */
-	@Around("@annotation( timedAnnotation ) ")
-	public Object processSystemRequest(final ProceedingJoinPoint pjp, Timed timedAnnotation) throws Throwable {		
-		try {
-			long start = System.currentTimeMillis();
-			Object retVal = pjp.proceed();
-			long end = System.currentTimeMillis();
-			long differenceMs = end - start;
+    private static Logger log = Logger.getLogger(TimingAspect.class);
 
-			final MethodSignature methodSignature = (MethodSignature) pjp.getSignature();
-			final Method targetMethod = methodSignature.getMethod();			
-			log.info(targetMethod.getDeclaringClass().getName() + "."
-					+ targetMethod.getName() + " took " + differenceMs
-					+ " ms : timing notes: " + timedAnnotation.timingNotes()
-					+ " request info : ");
-			
-			return retVal;
-			
-		} catch (Exception e) {
-			log.error(e);
-			throw e;
-		}
-	}
+    /**
+     * This around advice adds timing to any method annotated with the Timed
+     * annotation. It binds the annotation to the parameter timedAnnotation so
+     * that the values are available at runtime. Also note that the retention
+     * policy of the annotation needs to be RUNTIME.
+     *
+     * @param pjp             - the join point for this advice
+     * @param timedAnnotation - the Timed annotation as declared on the method
+     * @return
+     * @throws Throwable
+     */
+    @Around("@annotation( timedAnnotation ) ")
+    public Object processSystemRequest(final ProceedingJoinPoint pjp, Timed timedAnnotation) throws Throwable {
+        try {
+            long start = System.currentTimeMillis();
+            Object retVal = pjp.proceed();
+            long end = System.currentTimeMillis();
+            long differenceMs = end - start;
+
+            final MethodSignature methodSignature = (MethodSignature) pjp.getSignature();
+            final Method targetMethod = methodSignature.getMethod();
+
+            if (differenceMs > 100) {
+                log.warn(targetMethod.getDeclaringClass().getName() + "."
+                        + targetMethod.getName() + " took " + differenceMs
+                        + " ms : timing notes: " + timedAnnotation.timingNotes()
+                        + " request info : ");
+            } else {
+                log.debug(targetMethod.getDeclaringClass().getName() + "."
+                        + targetMethod.getName() + " took " + differenceMs
+                        + " ms : timing notes: " + timedAnnotation.timingNotes()
+                        + " request info : ");
+            }
+
+            return retVal;
+
+        } catch (Exception e) {
+            log.error(e);
+            throw e;
+        }
+    }
 
 }
