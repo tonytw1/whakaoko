33f97db389a775d01c5b75e6598d3c966b998568
Tony McCrae <tonymccrae@monzo.com>
2020-01-21 09:03:16 +0000

    Controllers pre filter geotagged items.

--- src/main/java/uk/co/eelpieconsulting/feedlistener/controllers/FeedItemPopulator.java
+++ src/main/java/uk/co/eelpieconsulting/feedlistener/controllers/FeedItemPopulator.java
@@ -7,9 +7,12 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 import uk.co.eelpieconsulting.feedlistener.daos.FeedItemDAO;
 import uk.co.eelpieconsulting.feedlistener.model.Channel;
+import uk.co.eelpieconsulting.feedlistener.model.FeedItem;
 import uk.co.eelpieconsulting.feedlistener.model.Subscription;
 
 import java.net.UnknownHostException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class FeedItemPopulator {
@@ -35,19 +38,24 @@ public class FeedItemPopulator {
 		populateFeedItems(username, channel, page, mv, field, MAX_FEED_ITEMS, q);
 	}
 	
-	public void populateFeedItems(String username, Channel channel, Integer page, ModelAndView mv, String field, Integer pageSize, String q) throws UnknownHostException, MongoException {		
+	void populateFeedItems(String username, Channel channel, Integer page, ModelAndView mv, String field, Integer pageSize, String q) throws UnknownHostException, MongoException {
 		final int pageSizeToUse = pageSize != null ? pageSize : MAX_FEED_ITEMS;
 		final int pageToUse = (page != null && page > 0) ? page : 1;
 		if (pageSizeToUse > MAX_FEED_ITEMS) {
 			throw new RuntimeException("Too many records requested");	// TODO use correct exception.
 		}
 
+		List<FeedItem> feedItems;
 		if (!Strings.isNullOrEmpty(q)) {
-			mv.addObject(field, feedItemDAO.searchChannelFeedItems(channel.getId(), pageSizeToUse, pageToUse, username, q));
-			return;
+			feedItems = feedItemDAO.searchChannelFeedItems(channel.getId(), pageSizeToUse, pageToUse, username, q);
+		} else {
+			feedItems = feedItemDAO.getChannelFeedItems(channel.getId(), pageSizeToUse, pageToUse, username);
 		}
-		
-		mv.addObject(field, feedItemDAO.getChannelFeedItems(channel.getId(), pageSizeToUse, pageToUse, username));
+
+		List<FeedItem> geotagged = feedItems.stream().filter(FeedItem::isGeoTagged).collect(Collectors.toList());
+		mv.addObject("geotagged", geotagged);
+
+		mv.addObject(field, feedItems);
 	}
 	
 }
