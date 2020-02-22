675616380bca8b6a37b64cc9a77607c0eb1d9957
Tony McCrae <tonymccrae@monzo.com>
2020-02-22 22:20:34 +0000

    Cricket wellington works if utf16 is overridden with utf8.

--- src/test/java/uk/co/eelpieconsulting/feedlistener/rss/FeedParserTest.java
+++ src/test/java/uk/co/eelpieconsulting/feedlistener/rss/FeedParserTest.java
@@ -1,10 +1,12 @@
 package uk.co.eelpieconsulting.feedlistener.rss;
 
+import com.sun.syndication.feed.synd.SyndEntry;
 import com.sun.syndication.feed.synd.SyndFeed;
 import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 
 import java.io.FileInputStream;
+import java.util.Iterator;
 
 import static org.junit.Assert.assertEquals;
 
@@ -26,6 +28,23 @@ public class FeedParserTest {
         SyndFeed syndFeed = new FeedParser().parseSyndFeed(input.getBytes());
 
         assertEquals("Latest News - St Vincent de Paul Society Wellington", syndFeed.getTitle());
+    }
+
+    @Test
+    public void whatsUpWithCricketWellingtonsFeed() throws Exception {
+        String input = IOUtils.toString(new FileInputStream(this.getClass().getClassLoader().getResource("cricketwellington.xml").getFile()));
+
+        SyndFeed syndFeed = new FeedParser().parseSyndFeed(input.getBytes());
+
+        assertEquals("Cricket Wellington", syndFeed.getTitle());
+        assertEquals(50, syndFeed.getEntries().size());
+
+
+        final Iterator<SyndEntry> feedItemsIterator = syndFeed.getEntries().iterator();
+        SyndEntry next = feedItemsIterator.next();
+
+        assertEquals("Big names return for Firebirds", next.getTitle());
+        assertEquals("21 Feb 2020 11:46:19 GMT", next.getPublishedDate().toGMTString());
     }
 
 }
