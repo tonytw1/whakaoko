470c7af9dd99b081ccb5a9966bea69aee6b9d086
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-09-03 08:58:53 +0100

    Imports.

--- src/main/java/uk/co/eelpieconsulting/feedlistener/daos/SubscriptionsDAO.java
+++ src/main/java/uk/co/eelpieconsulting/feedlistener/daos/SubscriptionsDAO.java
@@ -8,7 +8,6 @@ import dev.morphia.query.FindOptions;
 import dev.morphia.query.Query;
 import dev.morphia.query.Sort;
 import dev.morphia.query.experimental.filters.Filters;
-import dev.morphia.query.internal.MorphiaCursor;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
