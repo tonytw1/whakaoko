885c4f5b3a2e9d4c9ac7f8d7e73743bf0f962fa2
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-09-03 10:35:04 +0100

    Clean up.

--- src/main/java/uk/co/eelpieconsulting/feedlistener/controllers/ui/HomepageController.kt
+++ src/main/java/uk/co/eelpieconsulting/feedlistener/controllers/ui/HomepageController.kt
@@ -1,14 +1,13 @@
 package uk.co.eelpieconsulting.feedlistener.controllers.ui
 
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.servlet.ModelAndView
 
 @Controller
 class HomepageController {
 
-    @RequestMapping(value = ["/"], method = [RequestMethod.GET])
+    @GetMapping("/")
     fun homepage(): ModelAndView? {
         return ModelAndView("homepage")
     }
