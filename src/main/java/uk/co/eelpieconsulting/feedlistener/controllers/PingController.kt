6db7cb16eaa26472db103bcf8ce064dc02ae9281
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-09-03 14:13:45 +0100

    Ported to Kotlin.

---
+++ src/main/java/uk/co/eelpieconsulting/feedlistener/controllers/PingController.kt
@@ -0,0 +1,19 @@
+package uk.co.eelpieconsulting.feedlistener.controllers
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.servlet.ModelAndView
+import uk.co.eelpieconsulting.common.views.ViewFactory
+
+@Controller
+class PingController @Autowired constructor(val viewFactory: ViewFactory){
+
+    @GetMapping("/healthz")
+    fun ping(): ModelAndView? {
+        return ModelAndView(viewFactory.getJsonView()).
+        addObject("data", "ok")
+    }
+
+
+}
