9a7ac05a1abf830c1d6cc4e5cdaf5ba189e6acc7
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-09-05 23:44:32 +0100

    Move to kotlin src folder.

---
+++ src/main/kotlin/uk/co/eelpieconsulting/feedlistener/controllers/ui/CurrentUserService.kt
@@ -0,0 +1,27 @@
+package uk.co.eelpieconsulting.feedlistener.controllers.ui
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import uk.co.eelpieconsulting.feedlistener.daos.UsersDAO
+import uk.co.eelpieconsulting.feedlistener.model.User
+import javax.servlet.http.HttpServletRequest
+
+@Component
+class CurrentUserService @Autowired constructor(val request: HttpServletRequest, val usersDAO: UsersDAO) {
+
+    private val signedInUserAttribute = "signedInUser"
+
+    fun getCurrentUserUser(): User? {
+        val username = request.session.getAttribute(signedInUserAttribute)
+        if (username != null) {
+            return usersDAO.getByUsername(username as String)
+        } else {
+            return null;
+        }
+    }
+
+    fun setSignedInUser(user: User) {
+        request.getSession().setAttribute(signedInUserAttribute, user.username)
+    }
+
+}
