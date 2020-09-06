d6c17004e4069e3d4889f7b6b9df24f99e7c25dc
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-09-06 12:30:22 +0100

    Port to Kotlin.

---
+++ src/main/kotlin/uk/co/eelpieconsulting/feedlistener/controllers/ChannelsController.kt
@@ -0,0 +1,87 @@
+package uk.co.eelpieconsulting.feedlistener.controllers
+
+import com.google.common.base.Strings
+import org.apache.log4j.Logger
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.servlet.ModelAndView
+import org.springframework.web.servlet.view.RedirectView
+import uk.co.eelpieconsulting.common.views.ViewFactory
+import uk.co.eelpieconsulting.feedlistener.IdBuilder
+import uk.co.eelpieconsulting.feedlistener.UrlBuilder
+import uk.co.eelpieconsulting.feedlistener.daos.ChannelsDAO
+import uk.co.eelpieconsulting.feedlistener.daos.FeedItemDAO
+import uk.co.eelpieconsulting.feedlistener.daos.SubscriptionsDAO
+import uk.co.eelpieconsulting.feedlistener.daos.UsersDAO
+import uk.co.eelpieconsulting.feedlistener.model.Channel
+import uk.co.eelpieconsulting.feedlistener.model.FeedItemsResult
+import uk.co.eelpieconsulting.feedlistener.model.Subscription
+import javax.servlet.http.HttpServletResponse
+
+@Controller
+class ChannelsController @Autowired constructor(val usersDAO: UsersDAO, val channelsDAO: ChannelsDAO,
+                                                val viewFactory: ViewFactory, val subscriptionsDAO: SubscriptionsDAO,
+                                                val urlBuilder: UrlBuilder, val idBuilder: IdBuilder,
+                                                val feedItemPopulator: FeedItemPopulator, val feedItemDAO: FeedItemDAO) {
+
+    private val log = Logger.getLogger(ChannelsController::class.java)
+
+    private val X_TOTAL_COUNT = "X-Total-Count"
+
+    @RequestMapping(value = ["/{username}/channels"], method = [RequestMethod.GET])
+    fun channelsJson(@PathVariable username: String): ModelAndView? {
+        usersDAO.getByUsername(username)
+        log.info("Channels for user: $username")
+        return ModelAndView(viewFactory.getJsonView()).addObject("data", channelsDAO.getChannels(username))
+    }
+
+    @RequestMapping(value = ["/{username}/channels/{id}"], method = [RequestMethod.GET])
+    fun channel(@PathVariable username: String, @PathVariable id: String): ModelAndView? {
+        usersDAO.getByUsername(username)
+        val channel: Channel = channelsDAO.getById(username, id)
+        return ModelAndView(viewFactory.getJsonView()).addObject("data", channel)
+    }
+
+    @RequestMapping(value = ["/{username}/channels/{id}/subscriptions"], method = [RequestMethod.GET])
+    fun channelSubscriptions(@PathVariable username: String, @PathVariable id: String, @RequestParam(required = false) url: String?): ModelAndView? {
+        usersDAO.getByUsername(username)
+        val channel: Channel = channelsDAO.getById(username, id)
+        val subscriptionsForChannel: List<Subscription> = subscriptionsDAO.getSubscriptionsForChannel(username, channel.id, url)
+        val mv = ModelAndView(viewFactory.getJsonView())
+        mv.addObject("data", subscriptionsForChannel)
+        return mv
+    }
+
+    @RequestMapping(value = ["/{username}/channels/{id}/items"], method = [RequestMethod.GET])
+    fun channelJson(@PathVariable username: String, @PathVariable id: String,
+                    @RequestParam(required = false) page: Int?,
+                    @RequestParam(required = false) pageSize: Int?,
+                    @RequestParam(required = false) format: String,
+                    @RequestParam(required = false) q: String?,
+                    response: HttpServletResponse): ModelAndView? {
+        usersDAO.getByUsername(username)
+        val channel: Channel = channelsDAO.getById(username, id)
+        var mv: ModelAndView? = ModelAndView(viewFactory.getJsonView())
+        if (!Strings.isNullOrEmpty(format) && format == "rss") {    // TODO view factory could do this?
+            mv = ModelAndView(viewFactory.getRssView(channel.name, urlBuilder.getChannelUrl(channel), ""))
+        }
+        val results: FeedItemsResult = feedItemDAO.getChannelFeedItemsResult(username, channel, page, q, pageSize)
+        feedItemPopulator.populateFeedItems(results, mv, "data")
+        val totalCount = results.totalCount
+        response.addHeader(X_TOTAL_COUNT, java.lang.Long.toString(totalCount))
+        return mv
+    }
+
+    @RequestMapping(value = ["/{username}/channels"], method = [RequestMethod.POST])
+    fun addChannel(@PathVariable username: String, @RequestParam name: String?): ModelAndView? {
+        usersDAO.getByUsername(username)
+        val newChannel = Channel(idBuilder.makeIdFor(name), name, username)
+        channelsDAO.add(username, newChannel)
+        return ModelAndView(RedirectView(urlBuilder.getChannelUrl(newChannel)))
+    }
+
+}
