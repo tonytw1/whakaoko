14d2cb0af099d3fbe355f3c368a4859d3d038eae
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-09-07 16:46:40 +0100

    Clean up.

--- src/main/java/uk/co/eelpieconsulting/feedlistener/IdBuilder.java
+++ src/main/java/uk/co/eelpieconsulting/feedlistener/IdBuilder.java
@@ -5,11 +5,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class IdBuilder {
 
-	public String makeIdFor(String text) {
-		String result = text.toLowerCase().trim().replaceAll("\\s", "-");
-		result = result.replaceAll("[^\\-a-z0-9_]", "");
-		result = result.replaceAll("--+", "-");
-		return result;
-	}
+    public String makeIdFor(String text) {
+        return text.toLowerCase().trim().
+                replaceAll("\\s", "-").
+                replaceAll("[^\\-a-z0-9_]", "").
+                replaceAll("--+", "-");
+    }
 
 }
---
+++ src/test/java/uk/co/eelpieconsulting/feedlistener/IdBuilderTest.java
@@ -0,0 +1,13 @@
+package uk.co.eelpieconsulting.feedlistener;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class IdBuilderTest {
+
+    @Test
+    public void canGenerateHumanReadableIdFromText() {
+        assertEquals("the-quick-brown-fox", new IdBuilder().makeIdFor("The quick, brown fox."));
+    }
+}
