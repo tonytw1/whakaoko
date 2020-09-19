c3e14982df9d4645b0108087a9e7c9de3ff64608
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-09-19 23:41:52 +0100

    WIP - porting model classes to Kotlin.

--- src/main/java/uk/co/eelpieconsulting/feedlistener/model/Channel.kt
+++ src/main/java/uk/co/eelpieconsulting/feedlistener/model/Channel.kt
@@ -1,53 +1,25 @@
-package uk.co.eelpieconsulting.feedlistener.model;
+package uk.co.eelpieconsulting.feedlistener.model
 
-import dev.morphia.annotations.Entity;
-import dev.morphia.annotations.Id;
-import org.bson.types.ObjectId;
+import dev.morphia.annotations.Entity
+import dev.morphia.annotations.Id
+import org.bson.types.ObjectId
 
 @Entity("channels")
-public class Channel {
+class Channel {
 
-	@Id
-	ObjectId objectId;
+    @Id
+    var objectId: ObjectId? = null
+    var id: String = ""
+    var name: String = ""
+    var username: String = ""
 
-	private String id, name, username;
-	
-	public Channel() {
-	}
-	
-	public Channel(String id, String name, String username) {
-		this.id = id;
-		this.name = name;
-		this.username = username;
-	}
+    constructor(id: String, name: String, username: String) {
+        this.id = id
+        this.name = name
+        this.username = username
+    }
 
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	@Override
-	public String toString() {
-		return "Channel [id=" + id + ", name=" + name + ", username=" + username + "]";
-	}
-	
+    override fun toString(): String {
+        return "Channel [id=$id, name=$name, username=$username]"
+    }
 }
