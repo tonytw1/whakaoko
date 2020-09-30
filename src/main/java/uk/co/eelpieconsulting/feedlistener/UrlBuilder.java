685cd0f1f3ff41c91390f8bfb4788daa3376b965
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-09-30 10:56:11 +0100

    Formatting.

--- src/main/java/uk/co/eelpieconsulting/feedlistener/UrlBuilder.java
+++ src/main/java/uk/co/eelpieconsulting/feedlistener/UrlBuilder.java
@@ -11,54 +11,54 @@ import uk.co.eelpieconsulting.feedlistener.model.User;
 public class UrlBuilder {
 
     @Value("${base.url}")
-	private String baseUrl;
+    private String baseUrl;
 
-	public String getBaseUrl() {
-		return baseUrl;
-	}
-	
-	public String getInstagramCallbackUrl() {
-		return getBaseUrl() + "/instagram/callback";
-	}
-	
-	public String getTwitterCallback(String username) {
-		return getBaseUrl() + "/twitter/callback/" + username;
-	}
-	
-	public String getSubscriptionUrl(Subscription subscription) {
-		return getBaseUrl() + "/ui/subscriptions/" + subscription.getId();
-	}
-	
-	public String getSubscriptionUrl(String subscriptionId) {
-		return getBaseUrl() + "/ui/subscriptions/" + subscriptionId;
-	}
-	
-	public String getDeleteSubscriptionUrl(Subscription subscription) {
-		return getBaseUrl() + "/subscriptions/" + subscription.getId() + "/delete";
-	}
+    public String getBaseUrl() {
+        return baseUrl;
+    }
 
-	public String getReadSubscriptionUrl(Subscription subscription) {
-		return getBaseUrl() + "/" + subscription.getUsername() + "/subscriptions/" + subscription.getId() + "/read";
-	}
-	
-	public String getSubscriptionItemsUrl(String subscriptionId) {
-		return getBaseUrl() + "/subscriptions/" + subscriptionId;
-	}
-	
-	public String getChannelItemsUrl(String channelId) {
-		return getBaseUrl() + "/channels/" + channelId;
-	}
-	
-	public String getChannelUrl(Channel channel) {
-		return getBaseUrl() + "/ui/channels/" + channel.getId();
-	}
+    public String getInstagramCallbackUrl() {
+        return getBaseUrl() + "/instagram/callback";
+    }
 
-	public String getUserUrl() {
-		return getBaseUrl() + "/ui";
-	}
-	
-	public String getNewChannelUrl() {
-		return getBaseUrl() + "/ui/channels/new";
-	}
-	
+    public String getTwitterCallback(String username) {
+        return getBaseUrl() + "/twitter/callback/" + username;
+    }
+
+    public String getSubscriptionUrl(Subscription subscription) {
+        return getBaseUrl() + "/ui/subscriptions/" + subscription.getId();
+    }
+
+    public String getSubscriptionUrl(String subscriptionId) {
+        return getBaseUrl() + "/ui/subscriptions/" + subscriptionId;
+    }
+
+    public String getDeleteSubscriptionUrl(Subscription subscription) {
+        return getBaseUrl() + "/subscriptions/" + subscription.getId() + "/delete";
+    }
+
+    public String getReadSubscriptionUrl(Subscription subscription) {
+        return getBaseUrl() + "/" + subscription.getUsername() + "/subscriptions/" + subscription.getId() + "/read";
+    }
+
+    public String getSubscriptionItemsUrl(String subscriptionId) {
+        return getBaseUrl() + "/subscriptions/" + subscriptionId;
+    }
+
+    public String getChannelItemsUrl(String channelId) {
+        return getBaseUrl() + "/channels/" + channelId;
+    }
+
+    public String getChannelUrl(Channel channel) {
+        return getBaseUrl() + "/ui/channels/" + channel.getId();
+    }
+
+    public String getUserUrl() {
+        return getBaseUrl() + "/ui";
+    }
+
+    public String getNewChannelUrl() {
+        return getBaseUrl() + "/ui/channels/new";
+    }
+
 }
