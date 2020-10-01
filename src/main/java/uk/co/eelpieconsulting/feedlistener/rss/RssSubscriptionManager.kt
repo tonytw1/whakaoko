8c67e2b15de2b0f691c08450172b15a0b9fe4ff4
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-10-01 22:27:37 +0100

    Convert to Kotlin.

--- src/main/java/uk/co/eelpieconsulting/feedlistener/rss/RssSubscriptionManager.kt
+++ src/main/java/uk/co/eelpieconsulting/feedlistener/rss/RssSubscriptionManager.kt
@@ -1,36 +1,27 @@
-package uk.co.eelpieconsulting.feedlistener.rss;
+package uk.co.eelpieconsulting.feedlistener.rss
 
-import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import uk.co.eelpieconsulting.feedlistener.UnknownSubscriptionException;
-import uk.co.eelpieconsulting.feedlistener.daos.SubscriptionsDAO;
-import uk.co.eelpieconsulting.feedlistener.model.RssSubscription;
-import uk.co.eelpieconsulting.feedlistener.model.Subscription;
+import org.apache.log4j.Logger
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import uk.co.eelpieconsulting.feedlistener.daos.SubscriptionsDAO
+import uk.co.eelpieconsulting.feedlistener.model.RssSubscription
 
 @Component
-public class RssSubscriptionManager {
-	
-	private static final Logger log = Logger.getLogger(RssSubscriptionManager.class);
-	
-	private final SubscriptionsDAO subscriptionsDAO;
-	
-	@Autowired
-	public RssSubscriptionManager(SubscriptionsDAO subscriptionsDAO) {
-		this.subscriptionsDAO = subscriptionsDAO;
-	}
-	
-	public RssSubscription requestFeedSubscription(String url, String channel, String username) {
-		log.info("Requesting subscription to feed: " + url);
-		final RssSubscription newSubscription = new RssSubscription(url, channel, username);
-		if (subscriptionsDAO.subscriptionExists(newSubscription.getId())) {
-			RssSubscription existingSubscription = subscriptionsDAO.getByRssSubscriptionById(newSubscription.getId());
-			if (existingSubscription != null) {
-				return existingSubscription;
-			}
-		}
-		return newSubscription;
-	}
-	
+class RssSubscriptionManager @Autowired constructor(private val subscriptionsDAO: SubscriptionsDAO) {
+
+    private val log = Logger.getLogger(RssSubscriptionManager::class.java)
+
+    fun requestFeedSubscription(url: String, channel: String, username: String): RssSubscription {
+        log.info("Requesting subscription to feed: $url")
+        val newSubscription = RssSubscription(url, channel, username)
+        if (subscriptionsDAO.subscriptionExists(newSubscription.id)) {
+            val existingSubscription = subscriptionsDAO.getByRssSubscriptionById(newSubscription.id)
+            if (existingSubscription != null) {
+                return existingSubscription
+            }
+        }
+        // TODO who should save this
+        return newSubscription
+    }
+
 }
