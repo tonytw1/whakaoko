8edf09832db90b77f4ad80b1420146aa11ee294b
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-10-01 22:38:04 +0100

    Convert to Kotlin.

--- src/main/kotlin/uk/co/eelpieconsulting/feedlistener/model/RssSubscription.kt
+++ src/main/kotlin/uk/co/eelpieconsulting/feedlistener/model/RssSubscription.kt
@@ -1,37 +1,19 @@
-package uk.co.eelpieconsulting.feedlistener.model;
+package uk.co.eelpieconsulting.feedlistener.model
 
-import dev.morphia.annotations.Entity;
-import org.apache.commons.codec.digest.DigestUtils;
+import dev.morphia.annotations.Entity
+import org.apache.commons.codec.digest.DigestUtils
 
 @Entity("subscriptions")
-public class RssSubscription extends Subscription {
-	
-	private String url;
-	
-	public RssSubscription() {
-	}
+class RssSubscription : Subscription {
 
-	public RssSubscription(String url, String channelId, String username) {
-		this.setId(channelId + "-" + "feed-" + DigestUtils.md5Hex(url));
-		this.setChannelId(channelId);
-		this.setUsername(username);
-		this.url = url;
-	}
-	
-	public String getUrl() {
-		return url;
-	}
+    lateinit var url: String
 
-	public void setUrl(String url) {
-		this.url = url;
-	}
+    constructor() {}
+    constructor(url: String, channelId: String, username: String) {
+        id = channelId + "-" + "feed-" + DigestUtils.md5Hex(url)
+        this.channelId = channelId
+        this.username = username
+        this.url = url
+    }
 
-	@Override
-	public String toString() {
-		return "RssSubscription [url=" + url + ", getChannelId()="
-				+ getChannelId() + ", getLastRead()=" + getLastRead()
-				+ ", getLatestItemDate()=" + getLatestItemDate()
-				+ ", getName()=" + getName() + "]";
-	}
-	
 }
