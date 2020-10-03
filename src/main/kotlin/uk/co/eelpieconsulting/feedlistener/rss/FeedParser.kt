dd7629190f391dca51247afd9a5612b305ff76fa
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-10-03 11:24:10 +0100

    Convert to Kotlin.

--- src/main/kotlin/uk/co/eelpieconsulting/feedlistener/rss/FeedParser.kt
+++ src/main/kotlin/uk/co/eelpieconsulting/feedlistener/rss/FeedParser.kt
@@ -1,67 +1,58 @@
-package uk.co.eelpieconsulting.feedlistener.rss;
+package uk.co.eelpieconsulting.feedlistener.rss
 
-import com.sun.syndication.feed.synd.SyndFeed;
-import com.sun.syndication.io.FeedException;
-import com.sun.syndication.io.ParsingFeedException;
-import com.sun.syndication.io.SyndFeedInput;
-import org.apache.commons.io.IOUtils;
-import org.springframework.stereotype.Component;
-import org.xml.sax.InputSource;
-
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import com.sun.syndication.feed.synd.SyndFeed
+import com.sun.syndication.io.FeedException
+import com.sun.syndication.io.ParsingFeedException
+import com.sun.syndication.io.SyndFeedInput
+import org.apache.commons.io.IOUtils
+import org.springframework.stereotype.Component
+import org.xml.sax.InputSource
+import java.io.ByteArrayInputStream
+import java.io.InputStream
 
 @Component
-public class FeedParser {
-
-    public SyndFeed parseSyndFeed(byte[] bytes) throws FeedException {
-        final byte[] cleaned = cleanLeadingWhitespace(bytes);
-
-        try {
-            return parse(cleaned);
-
-        } catch (ParsingFeedException p) {
-            if (p.getMessage().contains("The entity \"nbsp\" was referenced, but not declared.")) {
-                byte[] withOutNbsp = new String(bytes).replaceAll("\\&nbsp;", " ").getBytes();
-                return parse(withOutNbsp);
-
-            } else if (p.getMessage().contains("Invalid XML: Error on line 1: Content is not allowed in prolog")) {
-                byte[] withUtf8 = new String(bytes).replaceAll("utf-16", "utf-8").getBytes();
-                return parse(withUtf8);
-
+class FeedParser {
+    @Throws(FeedException::class)
+    fun parseSyndFeed(bytes: ByteArray): SyndFeed {
+        val cleaned = cleanLeadingWhitespace(bytes)
+        return try {
+            parse(cleaned)
+        } catch (p: ParsingFeedException) {
+            if (p.message!!.contains("The entity \"nbsp\" was referenced, but not declared.")) {
+                val withOutNbsp = String(bytes).replace("\\&nbsp;".toRegex(), " ").toByteArray()
+                parse(withOutNbsp)
+            } else if (p.message!!.contains("Invalid XML: Error on line 1: Content is not allowed in prolog")) {
+                val withUtf8 = String(bytes).replace("utf-16".toRegex(), "utf-8").toByteArray()
+                parse(withUtf8)
             } else {
-                throw (p);
+                throw p
             }
         }
     }
 
-    private SyndFeed parse(byte[] bytes) throws FeedException {
-        final InputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
-        InputSource inputStream = new InputSource(byteArrayInputStream);
-        try {
-            SyndFeedInput syndFeedInput = new SyndFeedInput(false);
-            final SyndFeed syndFeed = syndFeedInput.build(inputStream);
-            IOUtils.closeQuietly(byteArrayInputStream);
-            return syndFeed;
-
-        } catch (FeedException e) {
-            IOUtils.closeQuietly(byteArrayInputStream);
-            throw (e);
+    @Throws(FeedException::class)
+    private fun parse(bytes: ByteArray): SyndFeed {
+        val byteArrayInputStream: InputStream = ByteArrayInputStream(bytes)
+        val inputStream = InputSource(byteArrayInputStream)
+        return try {
+            val syndFeedInput = SyndFeedInput(false)
+            val syndFeed = syndFeedInput.build(inputStream)
+            IOUtils.closeQuietly(byteArrayInputStream)
+            syndFeed
+        } catch (e: FeedException) {
+            IOUtils.closeQuietly(byteArrayInputStream)
+            throw e
         }
     }
 
-    private byte[] cleanLeadingWhitespace(byte[] bytes) {
-        try {
-            String string = new String(bytes);
-            if (string.trim().equals(string)) {
-                return bytes;
-            }
-            return string.trim().getBytes();
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+    private fun cleanLeadingWhitespace(bytes: ByteArray): ByteArray {
+        return try {
+            val string = String(bytes)
+            if (string.trim { it <= ' ' } == string) {
+                bytes
+            } else string.trim { it <= ' ' }.toByteArray()
+        } catch (e: Exception) {
+            throw RuntimeException(e)
         }
     }
-
 }
-
