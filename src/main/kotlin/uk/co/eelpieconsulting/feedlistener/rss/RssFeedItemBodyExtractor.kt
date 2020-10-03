3937a0f87488e574793bb513273cdbefc72ee238
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-10-03 23:49:28 +0100

    Port to Kotlin.

--- src/main/kotlin/uk/co/eelpieconsulting/feedlistener/rss/RssFeedItemBodyExtractor.kt
+++ src/main/kotlin/uk/co/eelpieconsulting/feedlistener/rss/RssFeedItemBodyExtractor.kt
@@ -1,38 +1,34 @@
-package uk.co.eelpieconsulting.feedlistener.rss;
+package uk.co.eelpieconsulting.feedlistener.rss
 
-import java.util.List;
-
-import org.apache.log4j.Logger;
-import org.springframework.stereotype.Component;
-
-import com.google.common.base.Strings;
-import com.sun.syndication.feed.synd.SyndContentImpl;
-import com.sun.syndication.feed.synd.SyndEntry;
+import com.google.common.base.Strings
+import com.sun.syndication.feed.synd.SyndContentImpl
+import com.sun.syndication.feed.synd.SyndEntry
+import org.apache.log4j.Logger
+import org.springframework.stereotype.Component
 
 @Component
-public class RssFeedItemBodyExtractor {
+class RssFeedItemBodyExtractor {
 
-	private static Logger log = Logger.getLogger(RssFeedItemBodyExtractor.class);
-	
-	@SuppressWarnings("unchecked")
-	public String extractBody(final SyndEntry syndEntry) {
-		final List<SyndContentImpl> contents = syndEntry.getContents();
-		if (!contents.isEmpty()) {
-			final String contentBody = contents.get(0).getValue();		
-			return contentBody;
-		}
-		
-		log.debug("No content body found; looking for description");
-		String body = getItemDescription(syndEntry);
-		if (!Strings.isNullOrEmpty(body)) {			
-			return body;
-		}
-						
-		return null;		
-	}
-	
-	private String getItemDescription(final SyndEntry syndEntry) {
-		return syndEntry.getDescription() != null ? syndEntry.getDescription().getValue() : null;
-	}
-	
+    private val log = Logger.getLogger(RssFeedItemBodyExtractor::class.java)
+
+    fun extractBody(syndEntry: SyndEntry): String? {
+        val contents = syndEntry.contents // contents returns an untyped list of SyndContentImpl
+        if (contents.isNotEmpty()) {
+            val firstItem = contents[0]
+            if (firstItem != null && firstItem is SyndContentImpl) {
+                return firstItem.value
+            }
+        }
+
+        log.debug("No content body found; looking for description")
+        val body = getItemDescription(syndEntry)
+        return if (!Strings.isNullOrEmpty(body)) {
+            body
+        } else null
+    }
+
+    private fun getItemDescription(syndEntry: SyndEntry): String? {
+        return syndEntry.description?.value
+    }
+
 }
