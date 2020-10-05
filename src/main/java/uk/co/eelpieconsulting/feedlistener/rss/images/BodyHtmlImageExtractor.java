4dae67440ec03ebc0d10d126cf3f7906455cf09c
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-10-05 11:47:27 +0100

    Restore tests; leaves questions about where url resovling and blocked image checks should sit.

--- src/main/java/uk/co/eelpieconsulting/feedlistener/rss/images/BodyHtmlImageExtractor.java
+++ src/main/java/uk/co/eelpieconsulting/feedlistener/rss/images/BodyHtmlImageExtractor.java
@@ -1,6 +1,7 @@
 package uk.co.eelpieconsulting.feedlistener.rss.images;
 
 import com.google.common.base.Strings;
+import com.sun.syndication.feed.synd.SyndEntry;
 import org.apache.log4j.Logger;
 import org.htmlparser.NodeFilter;
 import org.htmlparser.Parser;
@@ -11,18 +12,11 @@ import org.htmlparser.util.ParserException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.co.eelpieconsulting.feedlistener.rss.RssFeedItemBodyExtractor;
-import com.sun.syndication.feed.synd.SyndEntry;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Set;
 
 @Component
 public class BodyHtmlImageExtractor {
 
     private static Logger log = Logger.getLogger(BodyHtmlImageExtractor.class);
-
-    private final Set<String> blockedUrlSnippets = Set.of("http://stats.wordpress.com", "gravatar.com/avatar", "share_save_171_16");
 
     private final RssFeedItemBodyExtractor rssFeedItemBodyExtractor;
 
@@ -36,10 +30,7 @@ public class BodyHtmlImageExtractor {
         if (!Strings.isNullOrEmpty(itemBody)) {
             String imageSrc = extractImage(itemBody);
             if (!Strings.isNullOrEmpty(imageSrc)) {
-                imageSrc = ensureFullyQualifiedUrl(imageSrc, item.getLink());   // TODO push up?
-                if (imageSrc != null && !isBlockListedImageUrl(imageSrc)) {     // TODO push up?
-                    return imageSrc;
-                }
+                return imageSrc;
             }
         }
         return null;
@@ -71,35 +62,4 @@ public class BodyHtmlImageExtractor {
         return imageSrc;
     }
 
-    private String ensureFullyQualifiedUrl(final String imageSrc, String itemUrl) {
-        if (imageSrc.startsWith("/")) {
-            log.debug("Image src looks like a relative url; attempting to fully qualify");
-            try {
-                final URL itemUrlUrl = new URL(itemUrl);
-                final String fullyQualifiedImageSrc = itemUrlUrl.getProtocol() + "://" + itemUrlUrl.getHost() + imageSrc;
-
-                final URL fullyQualifiedImageSrcUrl = new URL(fullyQualifiedImageSrc);
-                log.debug("Referenced from root image src resolved to fully qualified url: " + fullyQualifiedImageSrcUrl.toExternalForm());
-                return fullyQualifiedImageSrcUrl.toExternalForm();
-
-            } catch (MalformedURLException e) {
-                log.warn("Item base url or generated referenced from root image src url is not well formed; returning null");
-                return null;
-            }
-
-        } else {
-            try {
-                URL imageSrcUrl = new URL(imageSrc);
-                return imageSrcUrl.toExternalForm();
-
-            } catch (MalformedURLException e) {
-                log.debug("Returning null fully qualified image url for: " + imageSrc + " / " + itemUrl);
-                return null;
-            }
-        }
-    }
-
-    private boolean isBlockListedImageUrl(String url) {
-        return blockedUrlSnippets.stream().anyMatch(url::contains);
-    }
 }
--- src/main/java/uk/co/eelpieconsulting/feedlistener/rss/images/RssFeedItemImageExtractor.java
+++ src/main/java/uk/co/eelpieconsulting/feedlistener/rss/images/RssFeedItemImageExtractor.java
@@ -5,10 +5,16 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Set;
+
 @Component
 public class RssFeedItemImageExtractor {
 
     private final static Logger log = Logger.getLogger(RssFeedItemImageExtractor.class);
+
+    private final Set<String> BLOCKED_URL_SNIPPETS = Set.of("http://stats.wordpress.com", "gravatar.com/avatar", "share_save_171_16");
 
     private final BodyHtmlImageExtractor bodyHtmlImageExtractor;
     private final MediaModuleImageExtractor mediaModuleImageExtractor;
@@ -29,11 +35,47 @@ public class RssFeedItemImageExtractor {
 
         String bodyHtmlImage = bodyHtmlImageExtractor.extractImageFrom(item);
         if (bodyHtmlImage != null) {
-            return bodyHtmlImage;
+            // Attempt to expand relative image paths to fully qualified urls.
+            String fullyQualifiedImagePath = ensureFullyQualifiedUrl(bodyHtmlImage, item.getLink());
+            if (fullyQualifiedImagePath != null && !isBlockListedImageUrl(fullyQualifiedImagePath)) { // TODO specific to HTML only?
+                return fullyQualifiedImagePath;
+            }
         }
 
         log.debug("No suitable media element image seen");
         return null;
+    }
+
+    private String ensureFullyQualifiedUrl(final String imageSrc, String itemUrl) {
+        if (imageSrc.startsWith("/")) {
+            log.debug("Image src looks like a relative url; attempting to fully qualify");
+            try {
+                final URL itemUrlUrl = new URL(itemUrl);
+                final String fullyQualifiedImageSrc = itemUrlUrl.getProtocol() + "://" + itemUrlUrl.getHost() + imageSrc;
+
+                final URL fullyQualifiedImageSrcUrl = new URL(fullyQualifiedImageSrc);
+                log.debug("Referenced from root image src resolved to fully qualified url: " + fullyQualifiedImageSrcUrl.toExternalForm());
+                return fullyQualifiedImageSrcUrl.toExternalForm();
+
+            } catch (MalformedURLException e) {
+                log.warn("Item base url or generated referenced from root image src url is not well formed; returning null");
+                return null;
+            }
+
+        } else {
+            try {
+                URL imageSrcUrl = new URL(imageSrc);
+                return imageSrcUrl.toExternalForm();
+
+            } catch (MalformedURLException e) {
+                log.debug("Returning null fully qualified image url for: " + imageSrc + " / " + itemUrl);
+                return null;
+            }
+        }
+    }
+
+    private boolean isBlockListedImageUrl(String url) {
+        return BLOCKED_URL_SNIPPETS.stream().anyMatch(url::contains);
     }
 
 }
