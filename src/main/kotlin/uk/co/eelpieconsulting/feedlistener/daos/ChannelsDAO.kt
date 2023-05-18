667b3fdec9c34403a24b07c124152d1a345fd766
Tony McCrae <tony@eelpieconsulting.co.uk>
2023-05-18 08:23:06 +0100

    Unused.

--- src/main/kotlin/uk/co/eelpieconsulting/feedlistener/daos/ChannelsDAO.kt
+++ src/main/kotlin/uk/co/eelpieconsulting/feedlistener/daos/ChannelsDAO.kt
@@ -4,8 +4,6 @@ import com.mongodb.MongoException
 import dev.morphia.query.FindOptions
 import dev.morphia.query.Sort
 import dev.morphia.query.experimental.filters.Filters
-import org.apache.logging.log4j.LogManager
-
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import uk.co.eelpieconsulting.feedlistener.model.Channel
@@ -13,8 +11,6 @@ import uk.co.eelpieconsulting.feedlistener.model.User
 
 @Component
 class ChannelsDAO @Autowired constructor(val dataStoreFactory: DataStoreFactory) {
-
-    private val log = LogManager.getLogger(ChannelsDAO::class.java)
 
     private val NAME_ASCENDING = Sort.ascending("name")
 
