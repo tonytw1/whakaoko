230d7fcb23ac1ab13f618919896c89b99e3ab845
Tony McCrae <tony@eelpieconsulting.co.uk>
2023-05-28 10:08:02 +0100

    Refactor; isolate the 'liveness' classifier ahead of introducing more.
    ie. A feed could be 'live' and 'frequent' or 'live' and 'infrequent'

--- src/main/kotlin/uk/co/eelpieconsulting/feedlistener/rss/classification/Classifier.kt
+++ src/main/kotlin/uk/co/eelpieconsulting/feedlistener/rss/classification/Classifier.kt
@@ -11,23 +11,22 @@ class Classifier {
     private val badHttpCodes = setOf(404, 401, -1)
 
     fun classify(subscription: RssSubscription): FeedStatus? {
+        return livenessStatus(subscription)
+    }
+
+    private fun livenessStatus(subscription: RssSubscription) =
         if (goodHttpCodes.contains(subscription.httpStatus) && subscription.error == null) {
-            return FeedStatus.ok
-        }
-
-        if (goodHttpCodes.contains(subscription.httpStatus) && subscription.error != null) {
-            return FeedStatus.broken
-        }
-
-        if (badHttpCodes.contains(subscription.httpStatus)) {
-            return if (subscription.latestItemDate == null) {
+            FeedStatus.ok
+        } else if (goodHttpCodes.contains(subscription.httpStatus) && subscription.error != null) {
+            FeedStatus.broken
+        } else if (badHttpCodes.contains(subscription.httpStatus)) {
+            if (subscription.latestItemDate == null) {
                 FeedStatus.gone
             } else {
                 if (DateTime(subscription.latestItemDate).plusDays(3).isAfterNow) FeedStatus.wobbling else FeedStatus.gone
             }
+        } else {
+            null
         }
-
-        return null
-    }
 
 }
