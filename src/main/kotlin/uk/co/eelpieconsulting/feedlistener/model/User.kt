1515eb1371c5476b962a5e42c811694f191202af
Tony McCrae <tony@eelpieconsulting.co.uk>
2023-05-28 09:57:33 +0100

    User can be promoted to a data class.

--- src/main/kotlin/uk/co/eelpieconsulting/feedlistener/model/User.kt
+++ src/main/kotlin/uk/co/eelpieconsulting/feedlistener/model/User.kt
@@ -6,13 +6,10 @@ import dev.morphia.annotations.Id
 import org.bson.types.ObjectId
 
 @Entity("users")
-class User(
-    @Id val objectId: ObjectId, val username: String,
+data class User(
+    @Id val objectId: ObjectId,
+    val username: String,
     @get:JsonIgnore val password: String? = null,
     @get:JsonIgnore var googleUserId: String? = null,
     @get:JsonIgnore var accessToken: String? = null
-) {
-    override fun toString(): String {
-        return "User(objectId=$objectId, username='$username')"
-    }
-}
+)
