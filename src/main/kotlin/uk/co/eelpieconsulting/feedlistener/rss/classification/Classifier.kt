55edda388e29a775214e22084c7d084d0c9e93b6
Tony McCrae <tony@eelpieconsulting.co.uk>
2023-05-28 19:59:38 +0100

    Spike; calculate the average gap between subscription posts as a possible classification signal.

--- src/main/kotlin/uk/co/eelpieconsulting/feedlistener/rss/classification/Classifier.kt
+++ src/main/kotlin/uk/co/eelpieconsulting/feedlistener/rss/classification/Classifier.kt
@@ -1,17 +1,49 @@
 package uk.co.eelpieconsulting.feedlistener.rss.classification
 
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics
+import org.apache.logging.log4j.LogManager
 import org.joda.time.DateTime
+import org.joda.time.Duration
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+import uk.co.eelpieconsulting.feedlistener.daos.FeedItemDAO
 import uk.co.eelpieconsulting.feedlistener.model.RssSubscription
+import uk.co.eelpieconsulting.feedlistener.model.Subscription
 
 @Component
-class Classifier {
+class Classifier  @Autowired constructor(private val feedItemDAO: FeedItemDAO)  {
+
+    private val log = LogManager.getLogger(Classifier::class.java)
 
     private val goodHttpCodes = setOf(200, 304)
     private val badHttpCodes = setOf(404, 401, -1)
 
     fun classify(subscription: RssSubscription): FeedStatus? {
+        val frequency = frequency(subscription)
         return livenessStatus(subscription)
+    }
+
+    fun frequency(subscription: Subscription): String? {
+        // Given a subscription estimate the frequency of posts by look at the gaps between it's previous posts
+
+        val subscriptionFeedItems = feedItemDAO.getSubscriptionFeedItems(subscription, 1, 20)
+        val feedItems = subscriptionFeedItems.feedsItems
+
+        val itemDates = feedItems.mapNotNull {
+            if (it.date != null) it.date else it.accepted
+        }
+        if (itemDates.size < 3) {
+            return null
+        }
+        val stats: DescriptiveStatistics = DescriptiveStatistics()
+
+        for (i: Int in 0 until feedItems.size - 1) {
+            val gap =  Duration(DateTime(itemDates[i + 1]), DateTime(itemDates[i])).toStandardHours().hours.toDouble()
+            stats.addValue(gap)
+        }
+        
+        log.info("Frequency stats for " + subscription.name + ": " + stats.mean + " " + stats.standardDeviation)
+        return "${stats.mean} / ${stats.standardDeviation}"
     }
 
     private fun livenessStatus(subscription: RssSubscription) =
--- src/test/kotlin/uk/co/eelpieconsulting/feedlistener/classification/ClassifierTest.kt
+++ src/test/kotlin/uk/co/eelpieconsulting/feedlistener/classification/ClassifierTest.kt
@@ -1,8 +1,15 @@
 package uk.co.eelpieconsulting.feedlistener.classification
 
+import org.bson.types.ObjectId
 import org.joda.time.DateTime
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import uk.co.eelpieconsulting.feedlistener.daos.FeedItemDAO
+import uk.co.eelpieconsulting.feedlistener.model.Category
+import uk.co.eelpieconsulting.feedlistener.model.FeedItem
+import uk.co.eelpieconsulting.feedlistener.model.FeedItemsResult
 import uk.co.eelpieconsulting.feedlistener.model.RssSubscription
 import uk.co.eelpieconsulting.feedlistener.rss.classification.Classifier
 import uk.co.eelpieconsulting.feedlistener.rss.classification.FeedStatus
@@ -10,13 +17,16 @@ import java.util.*
 
 class ClassifierTest {
 
-    private val classifier = Classifier()
+    private val feedItemDAO = Mockito.mock(FeedItemDAO::class.java)
+
+    private val classifier = Classifier(feedItemDAO)
 
     @Test
     fun http200WithNoErrorIsOk() {
         val subscription = RssSubscription("http://localhost/ok", UUID.randomUUID().toString(), "a-user")
         subscription.httpStatus = 200
         subscription.error = null
+        `when`(feedItemDAO.getSubscriptionFeedItems(subscription, 1, 20)).thenReturn(FeedItemsResult(emptyList(), 0L))
 
         val result = classifier.classify(subscription)
 
@@ -27,6 +37,7 @@ class ClassifierTest {
     fun http404IsGone() {
         val subscription = RssSubscription("http://localhost/gone", UUID.randomUUID().toString(), "a-user")
         subscription.httpStatus = 404
+        `when`(feedItemDAO.getSubscriptionFeedItems(subscription, 1, 20)).thenReturn(FeedItemsResult(emptyList(), 0L))
 
         val result = classifier.classify(subscription)
 
@@ -39,10 +50,49 @@ class ClassifierTest {
         val subscription = RssSubscription("http://localhost/gone", UUID.randomUUID().toString(), "a-user")
         subscription.latestItemDate = DateTime.now().minusDays(1).toDate()
         subscription.httpStatus = -1
+        `when`(feedItemDAO.getSubscriptionFeedItems(subscription, 1, 20)).thenReturn(FeedItemsResult(emptyList(), 0L))
 
         val result = classifier.classify(subscription)
 
         assertEquals(FeedStatus.wobbling, result)
     }
 
+    @Test
+    fun subscriptionWithRegularPostsShouldBeClassifiedAsFrequent() {
+        // ie. the feed reading service has lost it's own connection. We don't want a back off when reading resumes.
+        val subscription = RssSubscription("http://localhost/recent", UUID.randomUUID().toString(), "a-user")
+        subscription.latestItemDate = DateTime.now().minusDays(1).toDate()
+        subscription.httpStatus = 200
+
+        val feedsItems = listOf(
+            testFeedItemFor(subscription).copy(date = DateTime.now().minusDays(1).toDate()),
+            testFeedItemFor(subscription).copy(date = DateTime.now().minusDays(7).toDate()),
+            testFeedItemFor(subscription).copy(date = DateTime.now().minusDays(10).toDate())
+        )
+
+        `when`(feedItemDAO.getSubscriptionFeedItems(subscription, 1, 20)).thenReturn(FeedItemsResult(feedsItems, feedsItems.size.toLong()))
+
+        val frequency = classifier.frequency(subscription)
+
+        assertEquals("108.0 / 50.91168824543142", frequency)
+    }
+
+    private fun testFeedItemFor(subscription: RssSubscription, categories: List<Category>? = null): FeedItem {
+        val url = "http://localhost/" + UUID.randomUUID().toString()
+        return FeedItem(
+            ObjectId.get(),
+            UUID.randomUUID().toString(),
+            url,
+            null,
+            DateTime.now().toDate(),
+            DateTime.now().toDate(),
+            null,
+            null,
+            null,
+            subscription.id,
+            subscription.channelId,
+            categories,
+            DateTime.now().toDate()
+        )
+    }
 }
