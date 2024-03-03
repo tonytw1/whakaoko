f52427d7b7fa28d4c70ae8228af4eb5f8e98361a
Tony McCrae <tony@eelpieconsulting.co.uk>
2024-03-03 13:00:35 +0000

    Clean up; replace hand made redirect error flash with Spring RedirectAttributes.

--- src/main/kotlin/uk/co/eelpieconsulting/feedlistener/controllers/ui/SignInController.kt
+++ src/main/kotlin/uk/co/eelpieconsulting/feedlistener/controllers/ui/SignInController.kt
@@ -8,32 +8,44 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.servlet.ModelAndView
+import org.springframework.web.servlet.mvc.support.RedirectAttributes
 import org.springframework.web.servlet.view.RedirectView
 import uk.co.eelpieconsulting.feedlistener.controllers.CurrentUserService
 import uk.co.eelpieconsulting.feedlistener.daos.UsersDAO
 import uk.co.eelpieconsulting.feedlistener.passwords.PasswordHashing
 
 @Controller
-class SignInController @Autowired constructor(val request: HttpServletRequest,
-                                              private val usersDAO: UsersDAO,
-                                              val currentUserService: CurrentUserService,
-                                              private val passwordHashing: PasswordHashing) {
+class SignInController @Autowired constructor(
+    private val request: HttpServletRequest,
+    private val usersDAO: UsersDAO,
+    private val currentUserService: CurrentUserService,
+    private val passwordHashing: PasswordHashing
+) {
 
     private val log = LogManager.getLogger(SignInController::class.java)
 
     @GetMapping("/signin")
-    fun signinPrompt(session: HttpSession): ModelAndView {
-        return withSessionError(session, ModelAndView("signin"))
+    fun signinPrompt(session: HttpSession, redirectAttributes: RedirectAttributes): ModelAndView {
+        return ModelAndView("signin")
     }
 
     @PostMapping("/signin")
-    fun signin(username: String, password: String, session: HttpSession, request: HttpServletRequest): ModelAndView {
+    fun signin(
+        username: String,
+        password: String,
+        request: HttpServletRequest,
+        redirectAttributes: RedirectAttributes
+    ): ModelAndView {
         log.info("Signing in as: $username")
         val user = usersDAO.getByUsername(username)
         if (user != null) {
             val userPersistedHashedPassword = user.password
             if (userPersistedHashedPassword != null) {
-                if (userPersistedHashedPassword.isNotEmpty() && passwordHashing.matches(password, userPersistedHashedPassword)) {
+                if (userPersistedHashedPassword.isNotEmpty() && passwordHashing.matches(
+                        password,
+                        userPersistedHashedPassword
+                    )
+                ) {
                     currentUserService.setSignedInUser(user)
                     return redirectToSignedInUserUI(request)
                 } else {
@@ -41,17 +53,14 @@ class SignInController @Autowired constructor(val request: HttpServletRequest,
                 }
             }
         }
-        return redirectToSigninPromptWithError("We could not find a user with this username and password", session)
+        return redirectToSigninPromptWithError(
+            "We could not find a user with this username and password",
+            redirectAttributes
+        )
     }
 
-    private fun withSessionError(session: HttpSession, mv: ModelAndView): ModelAndView {
-        val error = session.getAttribute("error")
-        session.removeAttribute("error")
-        return mv.addObject("error", error)
-    }
-
-    private fun redirectToSigninPromptWithError(error: String, session: HttpSession): ModelAndView {
-        session.setAttribute("error", error)
+    private fun redirectToSigninPromptWithError(error: String, redirectAttributes: RedirectAttributes): ModelAndView {
+        redirectAttributes.addFlashAttribute("error", error);
         return redirectToSigninPrompt()
     }
 
@@ -62,7 +71,7 @@ class SignInController @Autowired constructor(val request: HttpServletRequest,
     private fun redirectToSignedInUserUI(request: HttpServletRequest): ModelAndView {
         val redirectUrl = request.session.getAttribute("redirect")
         request.session.removeAttribute("redirect")
-        return if (redirectUrl != null && redirectUrl is String ) {
+        return if (redirectUrl != null && redirectUrl is String) {
             ModelAndView(RedirectView(redirectUrl))
         } else {
             ModelAndView(RedirectView("/"))
