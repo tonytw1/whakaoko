15737f1db23bf2da03ac95e76304eeb1ddeade7a
Tony McCrae <tony@eelpieconsulting.co.uk>
2024-03-04 17:14:58 +0000

    Flash create user errors.

--- src/main/kotlin/uk/co/eelpieconsulting/feedlistener/controllers/ui/UsersUIController.kt
+++ src/main/kotlin/uk/co/eelpieconsulting/feedlistener/controllers/ui/UsersUIController.kt
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.servlet.ModelAndView
+import org.springframework.web.servlet.mvc.support.RedirectAttributes
+import org.springframework.web.servlet.view.RedirectView
 import uk.co.eelpieconsulting.common.views.ViewFactory
 import uk.co.eelpieconsulting.feedlistener.controllers.CurrentUserService
 import uk.co.eelpieconsulting.feedlistener.daos.ChannelsDAO
@@ -24,7 +26,8 @@ class UsersUIController @Autowired constructor(
     private val viewFactory: ViewFactory,
     private val passwordHashing: PasswordHashing,
     currentUserService: CurrentUserService,
-    request: HttpServletRequest) : WithSignedInUser(currentUserService, request) {
+    request: HttpServletRequest
+) : WithSignedInUser(currentUserService, request) {
 
     private val log = LogManager.getLogger(UsersUIController::class.java)
 
@@ -35,19 +38,19 @@ class UsersUIController @Autowired constructor(
 
     @PostMapping("/ui/newuser")
     fun submitNewUser(
-            @RequestParam(value = "username", required = true) username: String,
-            @RequestParam(value = "password", required = true) password: String
+        @RequestParam(value = "username", required = true) username: String,
+        @RequestParam(value = "password", required = true) password: String,
+        redirectAttributes: RedirectAttributes
     ): ModelAndView? {
         if (Strings.isNullOrEmpty(username)) {
-            throw RuntimeException("No username given")
+            return redirectToCreateUserPromptWithError("No username given", redirectAttributes)
         }
         if (usersDAO.getByUsername(username) != null) {
-            throw RuntimeException("Username is not available")
+            return redirectToCreateUserPromptWithError("Username is not available", redirectAttributes)
         }
-
         val isValidPassword = password.trim().length > 6
         if (!isValidPassword) {
-            throw  RuntimeException("Invalid password")
+            return redirectToCreateUserPromptWithError("Invalid password", redirectAttributes)
         }
 
         val newUser = User(ObjectId.get(), username, passwordHashing.hash(password))
@@ -57,11 +60,18 @@ class UsersUIController @Autowired constructor(
         return ModelAndView(viewFactory.jsonView).addObject("data", "ok")
     }
 
+    private fun redirectToCreateUserPromptWithError(
+        error: String,
+        redirectAttributes: RedirectAttributes
+    ): ModelAndView {
+        redirectAttributes.addFlashAttribute("error", error);
+        return ModelAndView(RedirectView("/ui/newuser"))
+    }
+
     @GetMapping("/")
     fun userhome(): ModelAndView {
         fun usersHomepage(user: User): ModelAndView {
-            return ModelAndView("userhome").
-            addObject("channels", channelsDAO.getChannelsFor(user))
+            return ModelAndView("userhome").addObject("channels", channelsDAO.getChannelsFor(user))
         }
         return forCurrentUser(::usersHomepage)
     }
